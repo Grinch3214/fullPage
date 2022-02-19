c69dda87865552ade6539e0c3cb6f5d02599f8d3
Maxim Hrichina <scorpiomax90@gmail.com>
2022-02-19 18:17:52 +0200

    add kostile

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,7 @@
 const burgerButton = document.querySelector('.header__burger');
 const headerSection = document.querySelector('.header');
 const menuLinks = document.querySelectorAll('.header__navigation-item');
-
+const headerContainer = document.querySelector('.header__container')
 
 let fullpage = new Swiper('.fullpage', {
     //my classes
@@ -32,6 +32,65 @@ let fullpage = new Swiper('.fullpage', {
     // allowTouchMove: false,
 
 });
+fullpage.on('slideChange', function () {
+    if(this.activeIndex === 4) {
+        headerContainer.classList.add('show')
+    } else {
+        headerContainer.classList.remove('show')
+    }
+});
+
+
+// (function() {
+//     'use strict';
+  
+//     const breakpoint = window.matchMedia( '(max-width:767px)' );
+//     let fullpage;
+  
+//     const breakpointChecker = function() {
+//       if ( breakpoint.matches === true ) {
+//           if ( fullpage !== undefined ) fullpage.destroy( true, true );
+//           return;
+//       } else if ( breakpoint.matches === false ) {
+//         return enableSwiper();
+//       }
+//     };
+  
+//     const enableSwiper = function() {
+//         fullpage = new Swiper('.fullpage', {
+//             wrapperClass: 'fullpage__wrapper',
+//                 slideClass: 'fullpage__screen',
+            
+//                 direction: "vertical",
+//                 slidesPerView: 'auto',
+            
+//                 keyboard: {
+//                     enabled: true,
+//                     onlyInViewport: true,
+//                     pageUpDown: true,
+//                 },
+            
+//                 mousewheel: {
+//                     sensitivity: 1,
+//                 },
+            
+//                 watchOverflow: true,
+            
+//                 speed: 800,
+            
+//                 observer: true,
+//                 observeParents: true,
+//                 observeSlideChildren: true,
+//       });
+//     };
+  
+//     breakpoint.addEventListener('change', breakpointChecker);
+  
+//     breakpointChecker();
+  
+//   })();
+
+
 
 (function() {
     const burgerList = headerSection.classList;
@@ -51,3 +110,4 @@ let fullpage = new Swiper('.fullpage', {
         })
     }
 })();
+
